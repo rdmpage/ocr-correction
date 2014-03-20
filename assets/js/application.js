b66430e70edef0484db251f7065c0f53f7b3b629
David Shorthouse <davidpshorthouse@gmail.com>
2014-03-20 07:42:22 +0100

    Cleaned some js

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,4 @@
-/*global jQuery, window, document, self, alert, PouchDB, Backbone, _ */
+/*global jQuery, window, document, self, alert, PouchDB, _ */
 var OCRCorrection = (function($) {
 
   "use strict";
@@ -86,7 +86,7 @@ var OCRCorrection = (function($) {
     },
     
     setUserDefaults: function(obj) {
-      if(!obj.userName) { obj.userName = "Anonymous" };
+      if(!obj.userName) { obj.userName = "Anonymous"; }
       if(!obj.userUrl) { obj.userUrl = "#"; }
       if(!obj.userAvatar) { obj.userAvatar = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCI+PHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSIjZWVlIj48L3JlY3Q+PHRleHQgdGV4dC1hbmNob3I9Im1pZGRsZSIgeD0iMzIiIHk9IjMyIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjEycHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9zdmc+"; }
     },
@@ -139,10 +139,6 @@ var OCRCorrection = (function($) {
 
     getTime: function() {
       return parseInt(String(new Date().getTime()).substring(0,10), 10);
-    },
-
-    formatHistoryItem: function(obj) {
-      return;
     },
 
     synchronize: function() {
