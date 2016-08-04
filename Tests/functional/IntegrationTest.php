2c1f02e38d0e75b6c733cf2e5d743611f6e56824
David Shorthouse <davidpshorthouse@gmail.com>
2016-08-04 00:10:13 -0400

    OMG Travis tests are now working

--- Tests/functional/IntegrationTest.php
+++ Tests/functional/IntegrationTest.php
@@ -32,7 +32,7 @@ class IntegrationTest extends TestCase {
   public function testPageTitle() {
     $this->webDriver->get($this->base_url);
     $title = $this->webDriver->getTitle();
-    $this->assertEquals('OCR Correction', $this->webDriver->getPageSource());
+    $this->assertEquals('OCR Correction', $title);
   }
 }
 ?>
